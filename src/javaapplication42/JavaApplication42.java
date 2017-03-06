7aa8565f8aaeb3fbab57ea59de3a08b518e27611
user <user@user-PC>
2017-03-06 12:25:07 +0100

    Segunda subida del proyecto

--- src/javaapplication42/JavaApplication42.java
+++ src/javaapplication42/JavaApplication42.java
@@ -16,6 +16,7 @@ public class JavaApplication42 {
      */
     public static void main(String[] args) {
         System.out.println("Hola mundo.");
+        System.out.println("Adios mundo.");
     }
     
 }
